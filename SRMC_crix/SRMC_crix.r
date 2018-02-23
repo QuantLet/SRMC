97508169923babffe46529db2e3e945fe8a903b4
lcx98 <32771821+lcx98@users.noreply.github.com>
2018-02-24 04:06:35 +0800

    Update
    Standardize the log returns by GARCH(1,1)

--- SRMC_crix/SRMC_crix.r
+++ SRMC_crix/SRMC_crix.r
@@ -1,32 +1,33 @@
 #' @usage  Reform the data and save it as CRIX data frame: date, price used for the empirical study
-#' @input  crix.json is already saved in the working directory (availabe at 'http://crix.hu-berlin.de')
-#' @output SRMC_crix.csv, the log returns of CRIX data.frame with date and price information
-#' 
-#' 
+#' @input  SRMC_crix.json is already saved in the working directory (availabe at 'http://crix.hu-berlin.de')
+#' @output SRMC_crix.csv, the log returns of CRIX data.frame with date and price information after 
+
+
+
 # clear variables and install necessary packages
 # set working directory by setwd('C:/Users/...')
 # clear variables and install package
 rm(list = ls(all = TRUE))
+
+# Uncomment to change your working directory
+# setwd()
+
+# Install required packages
 library("rjson")
+library(fGarch)
+
+# Get the daily log return of crix price 
+crix      = fromJSON("SRMC_crix.json")
+price.log.return  = diff(log(crix$price))
+price.log.return  = na.omit(price.log.return)
+
+#pre-white data with a GARCH model
+GARCHvola = garchFit(~garch(1, 1), data = price.log.return)
+crixwhite = price.log.return / volatility(GARCHvola)
+crixclean = crixwhite - mean(crixwhite)
 
 
-crix      = fromJSON(file = "SRMC_crix.json")
-size.crix = length(crix)
-n         = 1:size.crix
+CRIX = data.frame(date = crix$date[2:(length(price.log.return) + 1)], price = crixclean)
 
-price.crix = function(x, crix) 
-    price  = crix[[x]]$price
-date.crix  = function(x, crix) 
-  date     = crix[[x]]$date
-
-crix.price = sapply(n, price.crix, crix)
-crix.date  = sapply(n, date.crix, crix)
-# Get the sequence of price and date of the crix
-
-price.log.return = diff(log(crix.price))
-# Get the log return of he crix.price
-
-CRIX = data.frame(date = crix.date[2:size.crix], price = price.log.return)
-
-write.csv(CRIX, file = "SRMC_crix", row.names = FALSE) 
-# Save the log returns of CRIX as SRMC_crix.csv in the current working directory
+# uncomment to save the results
+# write.csv(CRIX, file = "SRMC_crix", row.names = FALSE) 
